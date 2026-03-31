05378e7a728126f2a8c6d867d0f89311fda0e560
Thayná Rie Shinoda <thayrieshinoda@gmail.com>
2026-03-31 20:49:39 -0300

    feat: configurando para deploy do frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const apiBaseUrl =
+  import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/";
+
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: apiBaseUrl,
 });
 
 api.interceptors.request.use((config) => {
